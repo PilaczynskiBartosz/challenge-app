30b05ed32276879681f302c6e5e1d18544572c7d
PilaczynskiBartosz <89969008+PilaczynskiBartosz@users.noreply.github.com>
2022-06-18 19:52:04 +0200

    Create day_3.cs

---
+++ homework/day_3.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace ChallangeApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Hello, {args[0]}");
+        }
+    }
+}
