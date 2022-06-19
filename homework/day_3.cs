772c0148073a466494ea55c3b1f135a3f6d8ff82
PilaczynskiBartosz <89969008+PilaczynskiBartosz@users.noreply.github.com>
2022-06-19 09:29:46 +0200

    Create day_3.cs

---
+++ homework/day_3.cs
@@ -0,0 +1,8 @@
+    class day_3
+    {
+        static void Main(string[] args) //print output with variable passed from the command line
+        {
+            Console.WriteLine($"Hello, {args[0]}");
+        }
+    }
+    
