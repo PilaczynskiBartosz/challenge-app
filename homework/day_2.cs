86eb60a20a536743acda0fdb9d73f689c652c914
PilaczynskiBartosz <89969008+PilaczynskiBartosz@users.noreply.github.com>
2022-06-19 09:27:15 +0200

    Update and rename homework.cs to homework/day_2.cs

---
+++ homework/day_2.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace ChallangeApp
+{
+    class day_2
+    {
+        static void Main() //print output
+        {
+            Console.WriteLine($"Hola, Captain!");
+        }
+    }
+}
