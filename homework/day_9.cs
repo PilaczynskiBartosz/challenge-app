37e9e2b52514553fe7edaf133457902afaed471c
PilaczynskiBartosz <89969008+PilaczynskiBartosz@users.noreply.github.com>
2022-06-21 11:53:43 +0200

    Create day_9.cs

---
+++ homework/day_9.cs
@@ -0,0 +1,19 @@
+public void AddGradeStr(string grade)
+        {
+            bool success = int.TryParse(grade, out int numberGrade);
+            if(success)
+            {
+                if(numberGrade >= 0 && numberGrade <= 100)
+                {
+                    this.grades.Add(numberGrade);
+                }
+                else
+                {
+                    Console.WriteLine($"Couldn't add {grade} to list. Invalid value");
+                }
+            }
+            else
+            {
+                Console.WriteLine($"Couldn't add {grade} to list. Invalid value");
+            }
+        }
